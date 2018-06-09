a9673ea7b4d726381e5739397b1d4f77a0354ed1
kotireddy <kotireddyare@gmail.com>
2018-06-09 12:22:36 +0530

    Spring Boot Micro Service Database Registry server and database communication done...

--- src/main/java/com/spring/boot/micro/db/controller/SampleController.java
+++ src/main/java/com/spring/boot/micro/db/controller/SampleController.java
@@ -1,10 +1,29 @@
 package com.spring.boot.micro.db.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.boot.micro.db.constant.QueryConstants;
+import com.spring.boot.micro.db.entity.Users;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @RestController
 public class SampleController {
 
+    private static final Logger LOGGER = LoggerFactory.
+                                            getLogger(SampleController.class);
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @GetMapping("/getUserByUsername/{username}")
+    public Users checkUserByName(@PathVariable("username") String username){
+        LOGGER.info("<<<<<<< LoadUserByUsername Configuration >>>>>>>" + username);
+        Users user = entityManager.createQuery(QueryConstants.USER_LOGIN_QUERY, Users.class)
+                .setParameter("username", username).getSingleResult();
+        return user;
+    }
 
 }
